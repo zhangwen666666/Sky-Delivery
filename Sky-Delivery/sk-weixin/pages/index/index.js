88d5dd2c6b6b81db305fc8c1a5b57aa810a3aa29
zhangwen666666 <1813505899@qq.com>
2025-01-14 15:10:10 +0800

    用户端登录

--- Sky-Delivery/sk-weixin/pages/index/index.js
+++ Sky-Delivery/sk-weixin/pages/index/index.js
@@ -5,6 +5,7 @@ Page({
     msg: 'hello world',
     nikeName:'',
     url:'',
+    code:'',
   },
 
   //定义方法，获取微信用户的头像和昵称
@@ -20,6 +21,31 @@ Page({
           nikeName: res.userInfo.nickName,
           url:res.userInfo.avatarUrl
         })
+      }
+    })
+  },
+
+  // 微信登录，获取微信用户的授权码
+  // 每次点击拿到的授权码是不一样的，一个授权码只能使用一次
+  // 通过授权码可以请求微信服务器，获取微信用户的openID
+  userLogin(){
+    wx.login({
+      success: (res) => {
+        console.log(res.code) // 打印授权码
+        this.setData({
+          code: res.code
+        })
+      },
+    })
+  },
+
+  // 发送请求，请求后端的接口
+  sendRequest(){
+    wx.request({
+      url: 'http://111.119.211.126/user/shop/status',
+      method: 'GET',
+      success: (res) => {
+        console.log(res.data)
       }
     })
   }
