55f013b6b188aa7a5c1fe3cff803dfc4f2f5b736
zhangwen666666 <1813505899@qq.com>
2025-01-17 18:07:24 +0800

    用户下单

---
+++ Sky-Delivery/sky-take-out/sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")
+@RequestMapping("/user/order")
+@Api(tags = "C端-订单接口")
+@Slf4j
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @ApiOperation("用户下单")
+    @PostMapping("/submit")
+    public Result<OrderSubmitVO> submit(OrdersSubmitDTO ordersSubmitDTO){
+        log.info("用户下单，参数为：{}", ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
