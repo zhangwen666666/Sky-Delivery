82ea8174b138a07783aa0831045976a6e434f051
zhangwen666666 <1813505899@qq.com>
2025-02-10 21:46:16 +0800

    WebSocket

---
+++ Sky-Delivery/sky-take-out/sky-server/src/main/java/com/sky/config/WebSocketConfiguration.java
@@ -0,0 +1,13 @@
+package com.sky.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+public class WebSocketConfiguration {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
